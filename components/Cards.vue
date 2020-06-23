cdd6700ed4cbad305bcd5172451c336dc908d847
Tentative <micoli.giacomo@gmail.com>
2020-06-23 22:54:40 +0200

    first-section card

--- components/Cards.vue
+++ components/Cards.vue
@@ -1,55 +1,46 @@
 <template>
-  <v-card class="mx-auto" max-width="500">
-    <v-system-bar color="indigo darken-2" dark>
-      <v-spacer></v-spacer>
-
-      <v-icon>mdi-window-minimize</v-icon>
-
-      <v-icon>mdi-window-maximize</v-icon>
-
-      <v-icon>mdi-close</v-icon>
-    </v-system-bar>
-
-    <v-toolbar color="indigo" dark>
+  <v-card max-width="400" class="mx-auto">
+    <v-app-bar id="card-1">
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Discover</v-toolbar-title>
+      <v-toolbar-title>DOG IS GOOD | L'ASILO PER CANI</v-toolbar-title>
 
       <v-spacer></v-spacer>
+    </v-app-bar>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-    </v-toolbar>
-
-    <v-container fluid>
+    <v-container>
       <v-row dense>
-        <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
-          <v-card>
-            <v-img
-              :src="card.src"
-              class="white--text align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="200px"
+        <v-col cols="12">
+          <v-card color="#385F73" dark>
+            <v-card-title class="headline">Unlimited music now</v-card-title>
+
+            <v-card-subtitle
+              >Listen to your favorite artists and albums whenever and wherever,
+              online and offline.</v-card-subtitle
             >
-              <v-card-title v-text="card.title"></v-card-title>
-            </v-img>
 
             <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
+              <v-btn text>Listen Now</v-btn>
             </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col v-for="(item, i) in items" :key="i" cols="12">
+          <v-card :color="item.color" dark>
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title
+                  class="headline"
+                  v-text="item.title"
+                ></v-card-title>
+
+                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+              </div>
+
+              <v-avatar class="ma-3" size="125" tile>
+                <v-img :src="item.src"></v-img>
+              </v-avatar>
+            </div>
           </v-card>
         </v-col>
       </v-row>
@@ -60,23 +51,29 @@
 <script>
 export default {
   data: () => ({
+    items: [
+      {
+        color: '#1F7087',
+        src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+        title: 'Supermodel',
+        artist: 'Foster the People'
+      },
+      {
+        color: '#952175',
+        src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
+        title: 'Halcyon Days',
+        artist: 'Ellie Goulding'
+      }
+    ],
     cards: [
       {
-        title: 'Pre-fab homes',
-        src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
-        flex: 12
-      },
-      {
-        title: 'Favorite road trips',
-        src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
-        flex: 6
-      },
-      {
-        title: 'Best airlines',
-        src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
-        flex: 6
+        title: ''
       }
     ]
   })
 }
 </script>
+
+<style lang="scss" scoped>
+@import '@/assets/cards.scss';
+</style>

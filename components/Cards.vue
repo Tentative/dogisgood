6c4a69ad349b272373dd1f612314cf4fc3dd8597
Tentative <micoli.giacomo@gmail.com>
2020-06-24 04:33:57 +0200

    card-4 style fix

--- components/Cards.vue
+++ components/Cards.vue
@@ -167,14 +167,6 @@
                       <v-spacer></v-spacer>
 
                       <v-btn icon>
-                        <v-icon>mdi-heart</v-icon>
-                      </v-btn>
-
-                      <v-btn icon>
-                        <v-icon>mdi-bookmark</v-icon>
-                      </v-btn>
-
-                      <v-btn icon>
                         <v-icon>mdi-share-variant</v-icon>
                       </v-btn>
                     </v-card-actions>

3216cdd1b4ba04fce887988103804f0d42167b2b
Tentative <micoli.giacomo@gmail.com>
2020-07-10 04:25:52 +0200

    breadcrumbs disabled

--- pages/_asilo.vue
+++ pages/_asilo.vue
@@ -56,17 +56,17 @@
         <v-icon>mdi-chevron-up</v-icon>
       </v-btn>
     </v-fab-transition>
-    <Breadcrumbs :levels="1" />
+    <!-- <Breadcrumbs :levels="1" /> -->
   </v-layout>
 </template>
 
 <script>
 import Logo from '@/components/Logo.vue'
-import Breadcrumbs from '@/components/Breadcrumbs.vue'
+// import Breadcrumbs from '@/components/Breadcrumbs.vue'
 export default {
   components: {
-    Logo,
-    Breadcrumbs
+    Logo
+    // Breadcrumbs
   },
 
   data() {

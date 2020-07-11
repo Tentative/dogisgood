b16d14a1be79da0c164ed4ebc87823652f49e017
Tentative <micoli.giacomo@gmail.com>
2020-07-11 17:55:21 +0200

    percorsi-home content fix

--- components/Cards.vue
+++ components/Cards.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="home">
     <v-card max-width="85vw" class="mx-auto">
       <v-expansion-panels>
         <v-expansion-panel v-for="(card, i) in cards" id="panel-1" :key="i">
@@ -94,44 +94,102 @@
           >
           <v-expansion-panel-content id="card-content-3">
             <v-container>
-              <v-row dense>
-                <v-col cols="12">
-                  <v-card color="#21f37e" light>
-                    <v-card-title class="headline"
-                      >I nostri percorsi</v-card-title
-                    >
-
-                    <v-card-subtitle
+              <v-layout id="top" column justify-center align-center>
+                <v-flex xs12 class="main-banner">
+                  <Logo />
+                  <div id="content-title" class="title text-center">
+                    Percorsi educativi
+                  </div>
+                  <div class="text-center">
+                    <v-sheet id="main-content" color="white"
                       >Il nostro è il metodo di educazione detto gentile, adatto
-                      a qualunque cane</v-card-subtitle
-                    >
-
-                    <v-card-actions>
-                      <v-btn text to="/percorsi/">Scopri</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-col>
-
-                <v-col v-for="(item, i) in items" :key="i" cols="12">
-                  <v-card :color="item.color" dark>
-                    <div class="d-flex flex-no-wrap justify-space-between">
-                      <div>
-                        <v-card-title
-                          class="headline"
-                          v-text="item.title"
-                        ></v-card-title>
-
-                        <v-card-subtitle v-text="item.artist"></v-card-subtitle>
-                      </div>
-
-                      <v-avatar class="ma-3" size="125" tile>
-                        <v-img :src="item.src"></v-img>
-                      </v-avatar>
+                      a qualunque cane, indipendentemente da razza, età, sesso e
+                      caratteristiche individuali, e personalizzato con uno
+                      studio attento dell’animale affinché possa offrire la
+                      massima efficacia. Durante un percorso di educazione, cane
+                      e proprietario imparano, insieme, gli esercizi di base
+                      utili nella vita quotidiana, come la condotta al
+                      guinzaglio e il richiamo, ma soprattutto a relazionarsi
+                      nel migliore dei modi, a risolvere piccoli e grandi
+                      problemi ed avere così una vita insieme più felice e
+                      semplice per entrambi.
+                    </v-sheet>
+                    <div class="media1">
+                      <v-img :src="asilo1" />
                     </div>
-                    <v-btn :block="true" :to="item.to">{{ item.button }}</v-btn>
-                  </v-card>
-                </v-col>
-              </v-row>
+                    <v-card
+                      id="percorsi-card"
+                      class="mx-auto"
+                      width="85vw"
+                      color="#26c6da"
+                      dark
+                      style="margin-bottom: 20px"
+                    >
+                      <v-card-title>
+                        <v-icon large left>
+                          mdi-paw
+                        </v-icon>
+                        <span class="title font-weight-light"
+                          >Puppy School</span
+                        >
+                      </v-card-title>
+
+                      <v-card-text class="headline font-weight-bold">
+                        Gli obiettivi di questo percorso sono diversi e
+                        contribuiscono tutti al benessere di cucciolo e
+                        proprietario e alla crescita serena del nostro piccolo
+                        amico.
+                      </v-card-text>
+
+                      <v-card-actions>
+                        <v-list-item class="grow">
+                          <v-list-item-avatar color="grey darken-3">
+                            <v-img class="elevation-6" :src="logoSrc"></v-img>
+                          </v-list-item-avatar>
+
+                          <v-list-item-content>
+                            <v-list-item-title></v-list-item-title>
+                          </v-list-item-content>
+
+                          <v-row align="center" justify="end">
+                            <v-btn
+                              outlined
+                              color="purple"
+                              text
+                              to="./puppy-school"
+                            >
+                              Scopri
+                            </v-btn>
+                          </v-row>
+                        </v-list-item>
+                      </v-card-actions>
+                    </v-card>
+                    <v-sheet id="main-content" color="white"
+                      >Se invece sei alla ricerca del tuo amico peloso, ti
+                      offriamo la migliore consulenza, competente e attenta, che
+                      ti guidi in un percorso di adozione maturo e responsabile.
+                    </v-sheet>
+                  </div>
+                </v-flex>
+                <v-fab-transition>
+                  <v-btn
+                    v-show="fab"
+                    id="to-top"
+                    v-scroll="onScroll"
+                    fixed
+                    dark
+                    fab
+                    bottom
+                    right
+                    small
+                    color="pink"
+                    @click="$vuetify.goTo('#top', 2500)"
+                  >
+                    <v-icon>mdi-chevron-up</v-icon>
+                  </v-btn>
+                </v-fab-transition>
+                <!-- <Breadcrumbs :levels="1" /> -->
+              </v-layout>
             </v-container>
           </v-expansion-panel-content>
         </v-expansion-panel>
@@ -214,6 +272,7 @@
 <script>
 export default {
   data: () => ({
+    logoSrc: require('@/static/logo.jpg'),
     show: false,
     text: 'i nostri partner:',
     imgSrc: require('@/static/struttura.jpg'),

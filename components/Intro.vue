9068b36a5751c55b21dbed87763428e9a28e6d45
Tentative <micoli.giacomo@gmail.com>
2020-07-24 16:51:21 +0200

    intro homepage style fix

--- components/Intro.vue
+++ components/Intro.vue
@@ -3,11 +3,13 @@
     <v-sheet id="intro" color="rgba(0,0,0,0)">
       <v-img class="intro_img" :src="banner" />
       <v-sheet class="intro_content" color="#5ea5ef" dark>
-        Il nostro cane è un compagno di vita e condivide con noi avventure e
-        tempo libero, ma gli impegni quotidiani, spesso, ci tengono fuori casa
-        per parecchie ore costringendo il nostro amico a rimanere solo e
-        recluso. L’idea del nostro asilo nasce proprio da una riflessione
-        intorno alle esigenze di entrambi.
+        <div class="title">
+          Il nostro cane è un compagno di vita e condivide con noi avventure e
+          tempo libero, ma gli impegni quotidiani, spesso, ci tengono fuori casa
+          per parecchie ore costringendo il nostro amico a rimanere solo e
+          recluso. L’idea del nostro asilo nasce proprio da una riflessione
+          intorno alle esigenze di entrambi.
+        </div>
       </v-sheet>
     </v-sheet>
   </div>

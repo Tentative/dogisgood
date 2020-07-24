c49766e01495a14fe8caeb3a1a7f0d110fec7fab
Tentative <micoli.giacomo@gmail.com>
2020-07-24 11:49:43 +0200

    mobile content fix

--- pages/percorsi/index.vue
+++ pages/percorsi/index.vue
@@ -1,67 +1,113 @@
 <template>
   <v-layout id="top" column justify-center align-center>
     <v-flex xs12 class="main-banner">
-      <Logo :src="percorsi1" />
-      <div id="content-title" class="title text-center">Percorsi educativi</div>
-      <div class="text-center">
-        <v-sheet id="main-content" color="white"
-          >Il nostro è il metodo di educazione detto gentile, adatto a qualunque
-          cane, indipendentemente da razza, età, sesso e caratteristiche
-          individuali, e personalizzato con uno studio attento dell’animale
-          affinché possa offrire la massima efficacia. Durante un percorso di
-          educazione, cane e proprietario imparano, insieme, gli esercizi di
-          base utili nella vita quotidiana, come la condotta al guinzaglio e il
-          richiamo, ma soprattutto a relazionarsi nel migliore dei modi, a
-          risolvere piccoli e grandi problemi ed avere così una vita insieme più
-          felice e semplice per entrambi.
-        </v-sheet>
+      <Logo :src="percorsi1" class="hidden-md-and-up" />
+      <section id="content_large" :style="style" class="hidden-sm-and-down">
+        <v-container style="background-color:rgba(0,0,0,0.6)">
+          <v-row id="main_row" justify="center">
+            <v-col :cols="12">
+              <div
+                id="content-title"
+                class="  text-center display-4"
+                style="color:white"
+              >
+                Percorsi educativi
+              </div>
+            </v-col>
+          </v-row>
+          <v-row id="main_row" justify="center">
+            <v-col :cols="12">
+              <div class="sub_heading text-center display-3 pa-10">
+                Il nostro è il metodo di educazione detto gentile, adatto a
+                qualunque cane.
+              </div>
+            </v-col>
+          </v-row>
+          <v-row id="main_row" justify="center"> </v-row>
+        </v-container>
+      </section>
+      <section id="content_description">
+        <v-row align="start" class="mx-auto">
+          <v-row style="width:100%">
+            <div id="content-title" class="title text-center">
+              Percorsi educativi
+            </div>
+          </v-row>
+          <v-col :cols="12" md="6" xs="12">
+            <div class="text-center">
+              <v-sheet id="main-content" color="#aae56d"
+                >Il nostro è il metodo di educazione detto gentile, adatto a
+                qualunque cane, indipendentemente da razza, età, sesso e
+                caratteristiche individuali, e personalizzato con uno studio
+                attento dell’animale affinché possa offrire la massima
+                efficacia. Durante un percorso di educazione, cane e
+                proprietario imparano, insieme, gli esercizi di base utili nella
+                vita quotidiana, come la condotta al guinzaglio e il richiamo,
+                ma soprattutto a relazionarsi nel migliore dei modi, a risolvere
+                piccoli e grandi problemi ed avere così una vita insieme più
+                felice e semplice per entrambi.
+              </v-sheet>
+            </div>
+          </v-col>
+          <v-col :cols="12" xs="12" md="6" style="height:100%">
+            <v-card
+              class="mx-auto"
+              color="#26c6da"
+              width="85vw"
+              dark
+              max-width="100%"
+              height="100%"
+            >
+              <v-card-title>
+                <v-icon large left>
+                  mdi-paw
+                </v-icon>
+                <span class="title font-weight-light">Puppy School</span>
+              </v-card-title>
+
+              <v-card-text class="headline font-weight-bold">
+                Gli obiettivi di questo percorso sono diversi e contribuiscono
+                tutti al benessere di cucciolo e proprietario e alla crescita
+                serena del nostro piccolo amico.
+              </v-card-text>
+
+              <v-card-actions>
+                <v-list-item class="grow">
+                  <v-list-item-avatar color="grey darken-3">
+                    <v-img class="elevation-6" :src="logoSrc"></v-img>
+                  </v-list-item-avatar>
+
+                  <v-list-item-content>
+                    <v-list-item-title>Dog Is Good</v-list-item-title>
+                  </v-list-item-content>
+
+                  <v-row align="center" justify="end">
+                    <v-btn outlined color="purple" text to="./puppy-school">
+                      Scopri
+                    </v-btn>
+                  </v-row>
+                </v-list-item>
+              </v-card-actions>
+            </v-card>
+            <v-card
+              color="#26c6da"
+              dark
+              style="margin-top:20px"
+              width="85vw"
+              class="mx-auto"
+            >
+              <v-card-subtitle class="headline font-weight-bold"
+                >Se invece sei alla ricerca del tuo amico peloso, ti offriamo la
+                migliore consulenza, competente e attenta, che ti guidi in un
+                percorso di adozione maturo e responsabile..</v-card-subtitle
+              >
+            </v-card>
+          </v-col>
+        </v-row>
         <div class="media1">
           <v-img :src="asilo1" />
         </div>
-        <v-card
-          class="mx-auto"
-          width="85vw"
-          color="#26c6da"
-          dark
-          max-width="400"
-        >
-          <v-card-title>
-            <v-icon large left>
-              mdi-paw
-            </v-icon>
-            <span class="title font-weight-light">Puppy School</span>
-          </v-card-title>
-
-          <v-card-text class="headline font-weight-bold">
-            Gli obiettivi di questo percorso sono diversi e contribuiscono tutti
-            al benessere di cucciolo e proprietario e alla crescita serena del
-            nostro piccolo amico.
-          </v-card-text>
-
-          <v-card-actions>
-            <v-list-item class="grow">
-              <v-list-item-avatar color="grey darken-3">
-                <v-img class="elevation-6" :src="logoSrc"></v-img>
-              </v-list-item-avatar>
-
-              <v-list-item-content>
-                <v-list-item-title>Dog Is Good</v-list-item-title>
-              </v-list-item-content>
-
-              <v-row align="center" justify="end">
-                <v-btn outlined color="purple" text to="./puppy-school">
-                  Scopri
-                </v-btn>
-              </v-row>
-            </v-list-item>
-          </v-card-actions>
-        </v-card>
-        <v-sheet id="main-content" color="white"
-          >Se invece sei alla ricerca del tuo amico peloso, ti offriamo la
-          migliore consulenza, competente e attenta, che ti guidi in un percorso
-          di adozione maturo e responsabile.
-        </v-sheet>
-      </div>
+      </section>
     </v-flex>
     <v-fab-transition>
       <v-btn
@@ -97,6 +143,10 @@ export default {
   data() {
     return {
       imgSrc: this.$route.name,
+      style:
+        'background:url(' +
+        require('@/static/struttura2.jpg') +
+        ');background-position:center ;background-size:cover',
       fab: false,
       offsetTop: 0,
       percorsi1: require('@/static/struttura2.jpg'),

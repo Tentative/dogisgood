4a375046374b0538b30a4a094ac347e177cd4bd5
Tentative <micoli.giacomo@gmail.com>
2020-07-28 02:44:31 +0200

    fix

--- components/Struttura.vue
+++ components/Struttura.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <v-sheet id="struttura" color="rgba(0,0,0,0)">
-      <v-sheet class="intro_struttura" color="#aae56d" light>
+      <v-sheet class="intro_struttura title" color="#aae56d" light>
         La struttura che ospita l’Asilo Dog is Good è stata realizzata per
         rispondere al meglio a tutte le esigenze dei nostri amici pelosi e
         offrire comfort, igiene e sicurezza adeguati al loro benessere. 400 mq e
         un ampio cortile privato a disposizione.
-        <v-btn depressed large color="primary">Info</v-btn>
+        <p style="text-align:center">
+          <v-btn depressed large color="primary">Info</v-btn>
+        </p>
       </v-sheet>
     </v-sheet>
   </div>

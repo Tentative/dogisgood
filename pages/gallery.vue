a4d76d79590e2c91e8f439c6ddd1c42d5deaf75b
Tentative <micoli.giacomo@gmail.com>
2020-08-27 04:31:44 +0200

    fix

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <section id="bautique" style="height:100vh">
-      <v-carousel height="100vh">
+      <v-carousel height="100vh" cycle interval="3000">
         <v-carousel-item
           v-for="(item, i) in items"
           :key="i"
